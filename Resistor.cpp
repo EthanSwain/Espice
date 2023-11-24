6d0dd06b655717bee8c550812d9d111e08015a5c
EthanSwain <Dswain1@umbc.edu>
2023-11-24 09:28:20 -0500

    finished the Resistor class

--- Resistor.cpp
+++ Resistor.cpp
@@ -1,4 +1,5 @@
 #include "Resistor.h"
+#include "Node.h"
 #include <iostream>
 
 Resistor::Resistor(int num_terminals,string name,string type, float resistance) 
@@ -18,14 +19,32 @@ void Resistor::set_resistance(float resistance){
     this->resistance = resistance;
 }
 
+void Resistor::set_voltage_drop(float volts){
+    this->voltage_drop = volts;
+}
+
+void Resistor::set_current_known(bool known){
+    this->current_is_known = known;
+}
+
 bool Resistor::calc_current(int high_term,int low_term){
     Node *high_node = get_node(high_term);
     Node *low_node  = get_node(low_term);
     if(high_node->get_known() && low_node->get_known()){
-        
-
+       float volt_diff = (high_node->get_volts() - low_node->get_volts());
+       float current = volt_diff/(this->resistance);
+       set_current(current);
         return true;
     } else {
+        return false;
+    }
+}
+
+bool Resistor::calc_voltage_drop(){
+    if(get_current_known()){
+        set_voltage_drop((get_current() * get_resistance()));
+        return true;
+    } else{
         return false;
     }
 }
--- Resistor.h
+++ Resistor.h
@@ -7,16 +7,21 @@ class Resistor : public Component{
     private:
         float resistance; // resistance of the resistor
         float current; //current through the resistor + is from 0->1
+        float voltage_drop;
+        bool current_is_known;
     public:
-    bool current_known;
     Resistor(int num_terminals  = 2,string name = "New Resistor",string type = "Resistor", float resistance = 0);
     ~Resistor();
+    void set_current_known(bool known);
+    bool get_current_known() const {return current_is_known;}
     void set_current(float current);
     float get_current() const {return current;}
     void set_resistance(float resistance);
     float get_resistance() const {return resistance;}
+    void set_voltage_drop(float volts);
+    float get_voltage_drop() const{return voltage_drop;}
     bool calc_current(int high_term,int low_term); //returns true if current can be calculated
-    float calc_voltage_drop(int known_terminal); // takes in known terminal positon and reutnr voltage drip
+    bool calc_voltage_drop(); // takes in known terminal positon and reutnr voltage drip
 };  
 
 #endif
